65e05fcf55185f9b25657662f1bdcfe2a9d657fa
Joseph Abrahamson <abrahamson.j@gmail.com>
2012-07-08 14:12:03 -0400

    * src/maybe_m.erl: Added some more convenience functions for handling maybe types

--- src/maybe_m.erl
+++ src/maybe_m.erl
@@ -15,6 +15,12 @@
 %%
 
 -module(maybe_m).
+-author('Joseph Abrahamson <me@jspha.com>').
+
+-export([just/1, nothing/0]).
+-export_type([t/1]).
+-export([okjust/1]).
+-export([default/2, then/2]).
 
 -behaviour(monad).
 -export(['>>='/2, return/1, fail/1]).
@@ -22,17 +28,60 @@
 -behaviour(monad_plus).
 -export([mzero/0, mplus/2]).
 
--ifdef(use_specs).
--type(monad(A) :: {'just', A} | nothing).
+
+-type(t(A) :: {'just', A} | nothing).
+%% The quintessential Maybe (maybe:t) type. It can be deconstructed
+%% manually in a pattern matching bind, but also other functions
+%% exposed here will help to manipulate it.
+
+-type(monad(A) :: t(A)).
 -include("monad_specs.hrl").
 -include("monad_plus_specs.hrl").
--endif.
 
-'>>='({just, X}, Fun) -> Fun(X);
-'>>='(nothing,  _Fun) -> nothing.
 
-return(X) -> {just, X}.
-fail(_X)  -> nothing.
+-spec
+%% @doc Wraps an object into a Maybe type.
+just(_) -> {just, _}.
+just(X) -> {just, X}.
+
+-spec
+%% @doc Wraps a "failure" into a Maybe type.
+nothing() -> nothing.
+nothing() -> nothing.
+
+
+%% Constructors
+
+-spec
+%% @doc Interprets an `{ok, X}' as `{just, X}' and everything else as
+%% `nothing'.
+okjust({ok, T}) -> t(T)
+ ;    (_) -> t(_).
+okjust({ok, X}) -> just(X);
+okjust({ok, X, _Info}) -> just(X);
+okjust(_Else) -> nothing().
+
+
+%% Handlers
+
+-spec
+%% @doc Returns a default value if Nothing.
+default(Maybe :: t(T), Default :: T) -> T.
+default({just, X}, _) -> X;
+default(nothing, X) -> X.
+
+-spec 
+then(Maybe :: t(T), fun((T) -> t(O))) -> t(O).
+then({just, X}, Fn) -> Fn(X);
+then(nothing, _) -> nothing.
+
+
+%% Monad interface
+
+'>>='(M, F) -> then(M, F).
+
+return(X) -> just(X).
+fail(_X)  -> nothing().
 
 mzero() -> nothing.
 mplus(nothing, Y) -> Y;
